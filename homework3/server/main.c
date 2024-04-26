512d8defaa5d05a93a4f2b5fd57f3199d253d7a3
mvleriN <ivlevaleksandr4@gmail.com>
2024-04-26 15:53:01 +0300

    theoretical material

--- homework3/server/main.c
+++ homework3/server/main.c
@@ -1,15 +1,16 @@
 #include "server.h"
 
 int main() {
-    int SocketFileDescriptor = CreateSocketServer(8888);
-    int ConnectionFileDescriptor = ConnectionAccept(SocketFileDescriptor);
+    while (1) {
+        int SocketFileDescriptor = CreateSocketServer(8888);
+        int ConnectionFileDescriptor = ConnectionAccept(SocketFileDescriptor);
+        HandleRRCConnectionRequest(ConnectionFileDescriptor);
+        SendRRCConnectionSetup(ConnectionFileDescriptor);
+        HandleRRCConnectionSetupComplete(ConnectionFileDescriptor);
 
-    HandleRRCConnectionRequest(ConnectionFileDescriptor);
-    SendRRCConnectionSetup(ConnectionFileDescriptor);
-    HandleRRCConnectionSetupComplete(ConnectionFileDescriptor);
-
-    close(ConnectionFileDescriptor);
-    close(SocketFileDescriptor);
+        close(ConnectionFileDescriptor);
+        close(SocketFileDescriptor);
+    }
 
     return 0;
 }
